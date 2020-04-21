5063a4d067a41f087653ad981af02e1bd06e1a26
Sebastian <sebastian@MacBookAir.local>
2020-04-21 21:04:33 +0900

    Added Kaleidoscope to work with HTTP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 //import {PanoViewer} from "@egjs/view360"
-
-
+declare  var Kaleidoscope:  any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +8,11 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class AppComponent {
 
+  viewer: any;
+  //Kaleidoscope = kaleidoscope;
+  test(){
+    var Kaleidoscope = kaleidoscope.value;
+  }
 
   newScrollPointX = 0; // 現在のスクロールX位置
   newScrollPointY = 0; // 現在のスクロールY位置
@@ -18,6 +22,7 @@ export class AppComponent {
   element2: any;
   @ViewChild('container') container: HTMLElement;
   @ViewChild('pano_video') videoEl: HTMLVideoElement;
+  //@ViewChild('container360') containerSelector;
 
   //container = document.querySelector(".container");
   //videoEl = document.getElementById("pano_video");
@@ -25,8 +30,27 @@ export class AppComponent {
 //     video: this.videoEl
 // });
 
-  constructor() { }
+  constructor() {
+  }
+
   
+  start() {
+     var containerSelector = '#container360';
+      this.viewer = new Kaleidoscope.Video({
+          source: '../../assets/cool.mp4',// './assets/cool.mp4',
+          containerId: containerSelector,
+          height: window.innerHeight,
+          width: window.innerWidth
+      });
+      this.viewer.render();
+      window.onresize = this.viewer.setSize({height: window.innerHeight, width: window.innerWidth});
+      
+      document.querySelector(containerSelector).addEventListener('touchend', this.viewer.play.bind(this.viewer));
+      document.body.addEventListener('click', function() {
+          this.viewer.play();
+      }.bind(this));
+
+  }
   openView() {
     // this.element2 = new PanoViewer(
     //   window.document.getElementById("myPanoViewer")
